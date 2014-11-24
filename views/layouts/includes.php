0ad5c134b2e6a1708f2bb1ba4283932a5e95a854
sebi <sebastian.poell@gmail.com>
2014-11-24 02:17:14 +0100

    Use classes for helpers.

--- views/layouts/includes.php
+++ views/layouts/includes.php
@@ -16,11 +16,11 @@
 		
 		<!-- Load Javascript -->
 		<script src="<?php echo BASE_PATH; ?>external/jhana/script.js"></script>
-		<?php foreach(recursive_glob('assets/js/*.js') as $javascript) ?>
+		<?php foreach(JhanaHelper::recursive_glob('assets/js/*.js') as $javascript) ?>
 			<script src="<?php echo $javascript; ?>"></script>
 		
 		<!-- Load CSS (recommend using less) -->
-		<?php foreach(recursive_glob('assets/css/*.css') as $css) ?>
+		<?php foreach(JhanaHelper::recursive_glob('assets/css/*.css') as $css) ?>
 			<link href="<?php echo $css; ?>" rel="stylesheet">
 		
 		<!-- Load Less (from cache if possible) -->
@@ -29,7 +29,7 @@
 			Less_Cache::$cache_dir = 'external/less/cache';
 			
 			$files = array();
-			foreach(recursive_glob('assets/css/*.less') as $less)
+			foreach(JhanaHelper::recursive_glob('assets/css/*.less') as $less)
 				$files[$less] = $less;
 		
 			$css_file_name = Less_Cache::Get($files);
