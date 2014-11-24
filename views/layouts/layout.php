a174c024929c6e59f4889181feb85dcaeae5c320
Sebastian Pöll <sebastian.poell@gmail.com>
2014-11-24 23:17:31 +0100

    Update layout.php

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -1,6 +1,7 @@
 
 <!-- 
 	Use this layout file for things that stay the same for each view, like Header, Footer, etc.
+	This whole file will be updated with Pjax.
 -->
 
 <?php require_once $view ?>
