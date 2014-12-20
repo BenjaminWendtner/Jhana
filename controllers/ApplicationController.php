2b422ff67b6dfbde35f1a8c8e2863ef9059dd0ed
Sebastian Pöll <sebastian.poell@gmail.com>
2014-12-20 11:13:06 +0100

    Update ApplicationController.php

--- controllers/ApplicationController.php
+++ controllers/ApplicationController.php
@@ -2,7 +2,7 @@
 
 	/**
 	 * Each other controller has to extend ApplicationController.
-	 * Use ApplicationController for defining general filters etc.
+	 * Use ApplicationController for defining general filters for example.
 	 */
 	class ApplicationController extends Controller {
 
